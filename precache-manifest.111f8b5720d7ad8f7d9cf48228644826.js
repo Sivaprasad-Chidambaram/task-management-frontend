9ca63c19e814126b5637bf08c57bf4235e2ab4f6
csivaprasadc <csivaprasadc@users.noreply.github.com>
2021-06-27 13:31:04 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.111f8b5720d7ad8f7d9cf48228644826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f0369f43d034436f5beca157b2f504",
+    "url": "https://Sivaprasad-Chidambaram.github.io/task-management-frontend\n/index.html"
+  },
+  {
+    "revision": "2ae7fa3e2cfa647d8595",
+    "url": "https://Sivaprasad-Chidambaram.github.io/task-management-frontend\n/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "f575b3ccf1b408b215fe",
+    "url": "https://Sivaprasad-Chidambaram.github.io/task-management-frontend\n/static/js/2.c91d6685.chunk.js"
+  },
+  {
+    "revision": "2ae7fa3e2cfa647d8595",
+    "url": "https://Sivaprasad-Chidambaram.github.io/task-management-frontend\n/static/js/main.84a89d9f.chunk.js"
+  },
+  {
+    "revision": "3a4c1daab51823c13146",
+    "url": "https://Sivaprasad-Chidambaram.github.io/task-management-frontend\n/static/js/runtime~main.f485250d.js"
+  }
+]);
